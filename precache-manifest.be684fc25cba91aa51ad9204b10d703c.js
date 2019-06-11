33db5e73a994796844674ea79bc3cc4fefdf4d0e
prezes2018 <szczuk2@interia.pl>
2019-06-11 13:56:52 +0200

    Updates

---
+++ precache-manifest.be684fc25cba91aa51ad9204b10d703c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f9e117b77ee237a5f7e14515820912",
+    "url": "/reactcalc/index.html"
+  },
+  {
+    "revision": "ff5c5c6f2ec7926423df",
+    "url": "/reactcalc/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8feedca2edd0014182d5",
+    "url": "/reactcalc/static/css/main.b3c261ec.chunk.css"
+  },
+  {
+    "revision": "ff5c5c6f2ec7926423df",
+    "url": "/reactcalc/static/js/2.9a0ea294.chunk.js"
+  },
+  {
+    "revision": "8feedca2edd0014182d5",
+    "url": "/reactcalc/static/js/main.c1fbdb66.chunk.js"
+  },
+  {
+    "revision": "f5b99387b4a1e1c3e3f8",
+    "url": "/reactcalc/static/js/runtime~main.89a77944.js"
+  }
+]);
